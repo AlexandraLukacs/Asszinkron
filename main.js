b2dc8e3b76d9cf59f3bf8ff078d1a1d133ab7a30
AlexandraLukacs <lszandika15@gmail.com>
2024-05-09 09:32:28 +0200

    fetch alap

---
+++ main.js
@@ -0,0 +1,28 @@
+import { getKeres } from "./asyncron.js";
+
+console.log("1. üzenet");
+
+getKeres("adatok.json", megjelenit1);
+
+console.log("2. üzenet");
+
+getKeres("https://www.boredapi.com/api/activity", megjelenitBored);
+getKeres("https://pokeapi.co/api/v2/pokemon/ditto", megjelenitPokemon);
+
+
+function megjelenit1(adat){
+    console.log(adat);
+}
+
+function megjelenitBored(adat){
+    $("body").append(`<h1>Mit csináljak, ha unatkozok? Tippek időtöltéshez: </h1>`)
+    $("body").append(`<h2>Tevékenység: ${adat.activity}</h2>`)
+    $("body").append(`<p>Minimum létszám: ${adat.participants}</p>`)
+    $("body").append(`<p>${adat.type}</p>`)
+}
+
+function megjelenitPokemon(adat){
+    $("body").append(`<h1>Pokemon</h1>`)
+    $("body").append(`<h2>Név: ${adat.name}</h2>`)
+    $("body").append(`<img src="${adat.sprites.front_default}" alt="">`)
+}
